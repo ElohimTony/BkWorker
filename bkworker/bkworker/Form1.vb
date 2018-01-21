8881f126f38e69184087ccf40b524819e8ddadd5
ElohimTony <35653159+ElohimTony@users.noreply.github.com>
2018-01-21 11:45:39 +0100

    Add Background Walker
    Background Walker Files

---
+++ bkworker/bkworker/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim sa As String
+    Dim cmd As SqlCommand
+    Dim dt As SqlDataReader
+
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) _
+                         Handles BackgroundWorker1.DoWork
+        ' Do some time-consuming work on this thread.
+
+        'Call RoomBilling()
+
+        Call testsync()
+
+
+    End Sub
+    Private Sub BackgroundWorker1_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) _
+ Handles BackgroundWorker1.RunWorkerCompleted
+        NotifyIcon1.BalloonTipText = "All Routine Completed"
+        NotifyIcon1.ShowBalloonTip(500)
+        System.Threading.Thread.Sleep(100000)
+
+
+    End Sub
+    Sub testsync()
+
+        NotifyIcon1.BalloonTipText = "synchronizing inventory as "
+        NotifyIcon1.ShowBalloonTip(500)
+        Try
+            con.Open()
+
+            sa = "INSERT INTO OPENQUERY(MYSQL, 'SELECT * FROM test') SELECT [id], [name], [title] FROM test WHERE NOT EXISTS (SELECT * FROM OPENQUERY(MYSQL,'select id as idem from test') WHERE idem = id)"
+            cmd = New SqlCommand(sa, con)
+            dt = cmd.ExecuteReader
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+
+    End Sub
+End Class
---
+++ bkworker/bkworker/Module2.vb
@@ -0,0 +1,66 @@
+﻿Module Module2
+
+        Public Plink As New System.Diagnostics.Process()
+
+
+
+        Public Function Plink_Start(ByVal Host As String, ByVal Username As String, ByVal Password As String, Optional ByVal Hidden As Boolean = True, Optional ByVal LocalPort As String = "3307", Optional ByVal RemoteHost As String = "localhost", Optional ByVal RemotePort As String = "3306") As Boolean
+
+            Plink.StartInfo.FileName = Application.StartupPath & "\plink.exe"
+
+            Plink.StartInfo.Arguments = "-ssh -l " & Username & " -pw " & Password & " -L " & LocalPort & ":" & RemoteHost & ":" & RemotePort & " -batch " & Host
+
+            If Hidden = True Then
+
+                Plink.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+
+            End If
+
+
+
+            Try
+
+                Plink.Start()
+
+                Return True
+
+            Catch ex As Exception
+
+                Return False
+
+            End Try
+
+        End Function
+
+
+
+        Public Sub Plink_Stop()
+
+            Try
+
+                If Plink.HasExited = False Then
+
+                    Plink.Kill()
+
+                End If
+
+            Catch ex As Exception
+
+            End Try
+
+        End Sub
+
+    End Module
+
+
+
+'    'Start with Plink hidden using local port 3307 
+
+'Plink_Start("example.com", "ssh_username", "ssh_password", true, "3307", "localhost", "3306") 
+
+
+
+'    'Plink can be started visibly for debugging purposes 
+
+'Plink_Start("example.com", "ssh_username", "ssh_password", false, "3307", "localhost", "3306") 
+'End Module
